ac3a2ef0202679f1b501fc4e868d069fc71d7a30
youtingkun <youtingkun@163.com>
2022-04-20 12:40:11 +0800

    feat: bottomtab 跳转

--- App.tsx
+++ App.tsx
@@ -1,17 +1,105 @@
-import React, {Component} from 'react';
 import {WebView} from 'react-native-webview';
+import * as React from 'react';
+import {Text, View, Button} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-// ...
-class App extends Component {
-  render() {
-    return (
-      <WebView
-        source={{
-          uri: 'https://test.youtingkun.com/#/collectionAccount?indentNo=GO220322724114',
-        }}
+function WebViewScreen() {
+  return (
+    <WebView
+      source={{
+        uri: 'https://test.youtingkun.com/#/collectionAccount?indentNo=GO220322724114',
+      }}
+    />
+  );
+}
+
+function DetailsScreen() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Details!</Text>
+    </View>
+  );
+}
+
+function HomeScreen({navigation}) {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Home screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
       />
-    );
-  }
+    </View>
+  );
+}
+
+function SettingsScreen({navigation}) {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Settings screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+const HomeStack = createNativeStackNavigator();
+
+function HomeStackScreen() {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" component={HomeScreen} />
+      <HomeStack.Screen name="Details" component={DetailsScreen} />
+    </HomeStack.Navigator>
+  );
+}
+
+const SettingsStack = createNativeStackNavigator();
+
+function SettingsStackScreen() {
+  return (
+    <SettingsStack.Navigator>
+      <SettingsStack.Screen name="Settings" component={SettingsScreen} />
+      <SettingsStack.Screen name="Details" component={DetailsScreen} />
+    </SettingsStack.Navigator>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = 'home';
+            } else if (route.name === 'Setting') {
+              iconName = 'setting';
+            } else if (route.name === 'WebView') {
+              iconName = 'creditcard';
+            }
+
+            // You can return any component that you like here!
+            return <AntDesign name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}>
+        <Tab.Screen name="Home" component={HomeStackScreen} />
+        <Tab.Screen name="WebView" component={WebViewScreen} />
+        <Tab.Screen name="Setting" component={SettingsStackScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default App;
